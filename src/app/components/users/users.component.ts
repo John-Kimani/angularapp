c01b2cb6e9e32adce65c409cb76f875de62f5a00
John-Kimani <kimwaweru100@gmail.com>
2022-02-13 02:11:58 +0300

    ng directive

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/User';
 
 @Component({
   selector: 'app-users',
@@ -7,10 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
+  users!: User[];
   constructor() { }
 
   ngOnInit(): void {
-    console.log('Hello');
+    this.users = [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        age: 30,
+        address:{
+            street: '50 Main st',
+            city: 'Boston',
+            state: 'MA',
+        }
+    },
+    {
+      firstName: 'Kevin',
+      lastName: 'Johnson',
+      age: 34,
+      address:{
+          street: '20 School st',
+          city: 'Lynn',
+          state: 'MA',
+      }
+  },
+  {
+    firstName: 'Karen',
+    lastName: 'Williams',
+    age: 26,
+    address:{
+        street: '55 Mill st',
+        city: 'Miami',
+        state: 'FL',
+    }
+}
+    ]
   }
 
 }
