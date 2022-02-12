c861311ff44a0eb2a8d58efe8e28c9bf7a195525
John-Kimani <kimwaweru100@gmail.com>
2022-02-13 00:37:54 +0300

    creates user property as an object, display it on html and creates a interface and maps it on the property

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -10,47 +10,30 @@ import { Component } from "@angular/core";
 export class UserCompoenent{
 
     //properties
-    firstName:string;
-    lastName: string;
-    age: number;
-    address;
+    user: User;
 
-    foo:any;
-    hasKids:boolean;
-    numberArray: number[];
-    stringArray: string[];
-    mixedArray: any[];
-    myTupple: [string, number, boolean];
-    unusable:void;
-    u: undefined;
-    n: null;
 
     //methods
     constructor(){
-        this.firstName = 'John';
-        this.lastName = 'Doe';
-        this.age = 30;
-        this.address = {
-            street: '50 Main Street',
-            city: 'Boston',
-            state: 'MA'
+        this.user = {
+            firstName: 'John',
+            lastName: 'Doe',
+            age: 30,
+            address:{
+                street: '50 Main st',
+                city: 'Boston',
+                state: 'MA',
+            }
         }
-        this.foo = true;
-        this.hasKids = true;
-        this.numberArray = [1,2,3];
-        this.stringArray = ['John', 'Jim'];
-        this.mixedArray = [ 'John', 5 , true];
-        this.myTupple = [ 'Heloo', 1, true];
-        this,this.unusable = undefined;
-        this.n = null;
-        this.u = undefined;
-        console.log(this.addNumbers(2, 3));
-    }
-    showAge(){
-        return this.age + 2;
     }
 
-    addNumbers(num1: number, num2: number):number{
-        return num1 + num2;
+}interface User {
+    firstName: string,
+    lastName: string,
+    age: number,
+    address: {
+        street: string,
+        city: string,
+        state: string,
     }
 }
