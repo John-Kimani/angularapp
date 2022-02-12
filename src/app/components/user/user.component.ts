51e09fc7ec2ab85e774905edd66bb08198665539
John-Kimani <kimwaweru100@gmail.com>
2022-02-12 23:40:49 +0300

    adds user.c.css file

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -2,8 +2,9 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-user',
-    templateUrl: './user.component.html'
+    templateUrl: './user.component.html',
     // template: '<h2>John Doe<h2>'
+    styleUrls: ['./user.component.css']
 })
 
 export class UserCompoenent{
