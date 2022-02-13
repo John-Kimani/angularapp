94737eedac775763f0d9464ed5d5c02e1b36ca24
John-Kimani <kimwaweru100@gmail.com>
2022-02-13 03:04:10 +0300

    adds navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserCompoenent } from './components/user/user.component';
 import { UsersComponent } from './components/users/users.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserCompoenent,
-    UsersComponent
+    UsersComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
